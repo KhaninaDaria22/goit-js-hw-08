7e6899b79546a1782bcc7208357a9b2b1cc706ad
KhaninaDaria22 <khanina_dasha@ukr.net>
2023-11-11 23:14:35 +0300

    commit 2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,6 +11,8 @@ const refs ={
 refs.form.addEventListener('submit', onFormSubmit);
 refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
 
+populateTextarea();
+
 function onFormSubmit(evt) {
     evt.preventDefault();
 
